94f06772c3c85ee737eee49b1b475d6d48172cf1
Cribbdo <cribbdo@gmail.com>
2019-08-11 08:57:29 +0200

    Update

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,8 +3,10 @@ import React from 'react';
 
 const Rank = ({ name, entries }) => {
     return (
-        <div className='white f3'>
-            {`${name} , your current rank is...`}
+        <div>
+            <div className='white f3'>
+                {`${name} , your current entry count is...`}
+            </div>
             <div className='white f1 '>
                 {entries}
             </div>
